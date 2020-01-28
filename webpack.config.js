96808f05645ecae1ede3799750e1d9de547bbc1c
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-01-28 18:52:49 +0900

    set up react

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,29 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const isDevelopment = true;
 module.exports = {
+  mode: "development",
   entry: "./src/app.js",
   output: {
     path: `${__dirname}/public/build`,
     filename: "bundle.js"
   },
+  optimization: {
+    minimize: false
+  },
   module: {
     rules: [
-      { test: /\.(png|jpg|gif)$/, loader: "url-loader" },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: "url-loader"
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {}
+        }
+      },
       {
         test: /\.s(a|c)ss$/,
         exclude: /\.module.(s(a|c)ss)$/,
